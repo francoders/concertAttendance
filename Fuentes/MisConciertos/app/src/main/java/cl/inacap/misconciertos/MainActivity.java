883425d644638bdabb64a82609f266d7676daff0
Francisco Corvalan <francisco22corvalan@gmail.com>
2020-10-01 14:01:40 -0300

    actualizacion
    actualizacion

--- Fuentes/MisConciertos/app/src/main/java/cl/inacap/misconciertos/MainActivity.java
+++ Fuentes/MisConciertos/app/src/main/java/cl/inacap/misconciertos/MainActivity.java
@@ -1,6 +1,8 @@
 package cl.inacap.misconciertos;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AlertDialogLayout;
 
 import android.os.Bundle;
 import android.view.View;
@@ -9,12 +11,19 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cl.inacap.misconciertos.dto.Concierto;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText artistaTxt;
+    private EditText entradaTxt;
     private Button agregarBtn;
     private ListView conciertosLv;
+    private List<Concierto> conciertos = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +31,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.artistaTxt = findViewById(R.id.artistaTxt);
-        this.agregarBtn = findViewById(R.id.agregarBtn);
+        this.entradaTxt = findViewById(R.id.entradaTxt);
         this.conciertosLv = findViewById(R.id.conciertosLv);
+        this.agregarBtn = findViewById(R.id.agregarBtn);
         this.agregarBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Agregado", Toast.LENGTH_SHORT).show();
+
+                List<String> errores = new ArrayList<>();
+                String artistaStr = artistaTxt.getText().toString().trim();
+                String entradaStr = entradaTxt.getText().toString().trim();
+                int entrada;
+
+                try {
+                    entrada = Integer.parseInt(entradaStr);
+                    if (entrada < 0){
+                        throw new NumberFormatException();
+                    }
+                }catch (NumberFormatException ex){
+                    errores.add("El valor tiene que ser mayor que 0");
+                }
+
+                if (errores.isEmpty()){
+                    //Ingresar valor
+
+                }else {
+                    mostrarErrores(errores);
+                }
+
+
+
             }
         });
+    }
+
+    private void mostrarErrores(List<String> errores) {
+        String mensaje = "";
+        for (String e: errores){
+            mensaje+= "-" + e + "\n";
+        }
+        AlertDialog.Builder AlertBuilder = new AlertDialog.Builder(MainActivity.this);
+        AlertBuilder.setTitle("Error al Ingresar").setMessage(mensaje)
+                .setPositiveButton("Aceptar", null).create().show();
+
     }
 }
---
+++ Fuentes/MisConciertos/app/src/main/java/cl/inacap/misconciertos/dto/Concierto.java
@@ -0,0 +1,49 @@
+package cl.inacap.misconciertos.dto;
+
+public class Concierto {
+    private String artista;
+    private int fechaEvento;
+    private String genero;
+    private int valorEntrada;
+    private int calificacion;
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public int getFechaEvento() {
+        return fechaEvento;
+    }
+
+    public void setFechaEvento(int fechaEvento) {
+        this.fechaEvento = fechaEvento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getValorEntrada() {
+        return valorEntrada;
+    }
+
+    public void setValorEntrada(int valorEntrada) {
+        this.valorEntrada = valorEntrada;
+    }
+
+    public int getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+}
